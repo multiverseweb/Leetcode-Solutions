929a8d723f2ce40f1a324173c72ffa199958ea17
Tejas Gupta <131871888+multiverseweb@users.noreply.github.com>
2024-06-14 01:23:05 +0530

    Create 409-Longest-Palindrome.py

---
+++ solutions/409-Longest-Palindrome.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestPalindrome(self,s):
+        from collections import Counter
+        count = Counter(s)
+        length = 0
+        odd_found = False
+        for char, freq in count.items():
+            if freq % 2 == 0:
+                length += freq
+            else:
+                length += freq - 1
+                odd_found = True
+        if odd_found:
+            length += 1
+        return length
