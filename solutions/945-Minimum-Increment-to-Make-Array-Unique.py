89b42c06a0ba714c1dad0069bf60976bc56ec20b
Tejas Gupta <131871888+multiverseweb@users.noreply.github.com>
2024-06-15 01:43:53 +0530

    Create 945-Minimum-Increment-to-Make-Array-Unique.py

---
+++ solutions/945-Minimum-Increment-to-Make-Array-Unique.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minIncrementForUnique(self,nums):
+        nums.sort()
+        m=0
+        for i in range(1, len(nums)):
+            if nums[i]<=nums[i - 1]:
+                increment=nums[i-1] - nums[i]+1
+                m+=increment
+                nums[i] = nums[i-1] + 1
+        return m
