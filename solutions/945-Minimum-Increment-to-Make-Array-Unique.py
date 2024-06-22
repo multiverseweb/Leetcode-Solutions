d6a561a75a13961be2ace89fea01ffe78927dbb0
Tejas Gupta <131871888+multiverseweb@users.noreply.github.com>
2024-06-23 04:11:36 +0530

    Medium

--- solutions/945-Minimum-Increment-to-Make-Array-Unique.py
+++ solutions/945-Minimum-Increment-to-Make-Array-Unique.py
@@ -3,8 +3,8 @@ class Solution:
         nums.sort()
         m=0
         for i in range(1, len(nums)):
-            if nums[i]<=nums[i - 1]:
+            if nums[i]<=nums[i-1]:
                 increment=nums[i-1] - nums[i]+1
                 m+=increment
-                nums[i] = nums[i-1] + 1
+                nums[i] = nums[i-1]+1
         return m
